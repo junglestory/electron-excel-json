b2b2a3d1f21f2cf463691ae12a659dfa5d9e3780
김덕규 <monocaine@gmail.com>
2018-12-09 21:44:17 +0900

    Add auto update

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 'use strict'
 const electron = require('electron')
+const autoUpdater = require('electron-updater').autoUpdater
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
+const excelToJson = require('convert-excel-to-json');
 
 let mainWindow
 let contents
@@ -21,7 +23,37 @@ function createWindow() {
   }))
 
   contents = mainWindow.webContents
-  contents.openDevTools()
+  // contents.openDevTools()
+  autoUpdater.checkForUpdates()
 }
 
 app.on('ready', createWindow)
+
+autoUpdater.on('update-available', function () {
+  console.log('A new update is available')
+  contents.send('updater-message', 'A new update is available')
+})
+autoUpdater.on('checking-for-update', function () {
+  console.log('Checking-for-update')
+  contents.send('updater-message', 'Checking for Update..')
+})
+autoUpdater.on('error', function (error) {
+  console.log('error')
+  console.error(error)
+  contents.send('updater-message', 'Got Error')
+})
+autoUpdater.on('download-progress', function (bytesPerSecond, percent, total, transferred) {
+  console.log(`${bytesPerSecond}, ${percent}, ${total}, ${transferred}`)
+  contents.send('updater-message', `download progress : ${bytesPerSecond}, ${percent}, ${total}, ${transferred}`)
+})
+autoUpdater.on('update-downloaded', function (event) {
+  console.log('update-downloaded')
+  console.log(event)
+  contents.send('updater-message', 'update-downloaded')
+  autoUpdater.quitAndInstall();
+})
+
+autoUpdater.on('update-not-available', function () {
+  console.log('update-not-available')
+  contents.send('updater-message', 'update-not-available')
+})
