8e3147ed24f26eeb6ca726ab0cb8eaf9b1033f00
김덕규 <monocaine@gmail.com>
2018-12-10 22:42:24 +0900

    Delete installer.sh

--- installer.sh
+++
@@ -1,7 +0,0 @@
-# delete older files
-rm -rf dist
-
-# create mac app and dmg
-electron-packager ./ electron-excel-json --platform=darwin --arch x64 --out dist --prune
-
-electron-installer-dmg ./dist/electron-excel-json-darwin-x64/electron-excel-json.app electron-excel-json --out=./dist
