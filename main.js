b89e5bc2818da20dd3356896efeed174b5296212
김덕규 <monocaine@gmail.com>
2019-01-20 23:02:25 +0900

    Fix auto update

--- main.js
+++ main.js
@@ -1,152 +1,139 @@
-'use strict'
-const os = require('os');
-const electron = require('electron')
-const autoUpdater = require('electron-updater').autoUpdater
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
-const path = require('path')
-const url = require('url')
-const excelToJson = require('convert-excel-to-json');
-const squirrelUrl = "https://electron-excel-json.herokuapp.com";
-const version = app.getVersion();
-const platform = os.platform() + '_' + os.arch();
+const {app, BrowserWindow, Menu, protocol, ipcMain} = require('electron');
+const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
 
-let mainWindow
-let contents
+//-------------------------------------------------------------------
+// Logging
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This logging setup is not required for auto-updates to work,
+// but it sure makes debugging easier :)
+//-------------------------------------------------------------------
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600
+//-------------------------------------------------------------------
+// Define the menu
+//
+// THIS SECTION IS NOT REQUIRED
+//-------------------------------------------------------------------
+let template = []
+if (process.platform === 'darwin') {
+  // OS X
+  const name = app.getName();
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: 'About ' + name,
+        role: 'about'
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() { app.quit(); }
+      },
+    ]
   })
-
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  contents = mainWindow.webContents
-  // contents.openDevTools()
-
-  // Add this condition to avoid error when running your application locally
-  if (process.env.NODE_ENV !== "dev") startAutoUpdater(squirrelUrl + '/update/' + platform + '/'+ version )
-  // autoUpdater.checkForUpdates()
 }
 
-app.on('ready', createWindow)
 
-const startAutoUpdater = (squirrelUrl) => {
-  // The Squirrel application will watch the provided URL
-  electron.autoUpdater.setFeedURL(`${squirrelUrl}`);
-console.log("squirrelUrl : " + `${squirrelUrl}`);
+//-------------------------------------------------------------------
+// Open a window that displays the version
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This isn't required for auto-updates to work, but it's easier
+// for the app to show a window than to have to click "About" to see
+// that updates are working.
+//-------------------------------------------------------------------
+let win;
 
-autoUpdater.on('update-available', function () {
-  console.log('A new update is available')
-  contents.send('updater-message', 'A new update is available')
-})
-autoUpdater.on('checking-for-update', function () {
-  console.log('Checking-for-update')
-  contents.send('updater-message', 'Checking for Update..')
-})
-autoUpdater.on('error', function (error) {
-  console.log('error')
-  console.error(error)
-  contents.send('updater-message', 'Got Error')
-})
-autoUpdater.on('download-progress', function (bytesPerSecond, percent, total, transferred) {
-  console.log(`${bytesPerSecond}, ${percent}, ${total}, ${transferred}`)
-  contents.send('updater-message', `download progress : ${bytesPerSecond}, ${percent}, ${total}, ${transferred}`)
-})
-autoUpdater.on('update-downloaded', function (event) {
-  console.log('update-downloaded')
-  console.log(event)
-  contents.send('updater-message', 'update-downloaded')
-  autoUpdater.quitAndInstall();
-})
-
-autoUpdater.on('update-not-available', function () {
-  console.log('update-not-available')
-  contents.send('updater-message', 'update-not-available')
-})
-
-  // Display a success message on successful update
-  // electron.autoUpdater.addListener("update-downloaded", (event, releaseNotes, releaseName) => {
-  //   electron.dialog.showMessageBox({"message": `The release ${releaseName} has been downloaded`});
-  // });
-
-  // // Display an error message on update error
-  // electron.autoUpdater.addListener("error", (error) => {
-  //   electron.dialog.showMessageBox({"message": "Auto updater error: " + error});
-  // });
-
-  // tell squirrel to check for updates
-  electron.autoUpdater.checkForUpdates();
- 
-
- 
-  /* Log whats happening
-  TODO send autoUpdater events to renderer so that we could console log it in developer tools
-  You could alsoe use nslog or other logging to see what's happening */
-  // autoUpdater.on('error', err => console.log(err));
-  // autoUpdater.on('checking-for-update', () => console.log('checking-for-update'));
-  // autoUpdater.on('update-available', () => console.log('update-available'));
-  // autoUpdater.on('update-not-available', () => console.log('update-not-available'));
-
-  
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
 }
-
-const handleSquirrelEvent = () => {
-  if (process.argv.length === 1) {
-    return false;
-  }
-
-  const squirrelEvent = process.argv[1];
-  switch (squirrelEvent) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
-    case '--squirrel-uninstall':
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case '--squirrel-obsolete':
-      app.quit();
-      return true;
-  }
+function createDefaultWindow() {
+  win = new BrowserWindow();
+  win.webContents.openDevTools();
+  win.on('closed', () => {
+    win = null;
+  });
+  win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+  return win;
 }
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
+app.on('ready', function() {
+  // Create the Menu
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
-if (handleSquirrelEvent()) {
-  // squirrel event handled and app will exit in 1000ms, so don't do anything else
-  return;
-}
+  createDefaultWindow();
+});
+app.on('window-all-closed', () => {
+  app.quit();
+});
 
-/*
-autoUpdater.on('update-available', function () {
-  console.log('A new update is available')
-  contents.send('updater-message', 'A new update is available')
-})
-autoUpdater.on('checking-for-update', function () {
-  console.log('Checking-for-update')
-  contents.send('updater-message', 'Checking for Update..')
-})
-autoUpdater.on('error', function (error) {
-  console.log('error')
-  console.error(error)
-  contents.send('updater-message', 'Got Error')
-})
-autoUpdater.on('download-progress', function (bytesPerSecond, percent, total, transferred) {
-  console.log(`${bytesPerSecond}, ${percent}, ${total}, ${transferred}`)
-  contents.send('updater-message', `download progress : ${bytesPerSecond}, ${percent}, ${total}, ${transferred}`)
-})
-autoUpdater.on('update-downloaded', function (event) {
-  console.log('update-downloaded')
-  console.log(event)
-  contents.send('updater-message', 'update-downloaded')
-  autoUpdater.quitAndInstall();
-})
+//
+// CHOOSE one of the following options for Auto updates
+//
 
-autoUpdater.on('update-not-available', function () {
-  console.log('update-not-available')
-  contents.send('updater-message', 'update-not-available')
-})
-*/
+//-------------------------------------------------------------------
+// Auto updates - Option 1 - Simplest version
+//
+// This will immediately download an update, then install when the
+// app quits.
+//-------------------------------------------------------------------
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+//-------------------------------------------------------------------
+// Auto updates - Option 2 - More control
+//
+// For details about these events, see the Wiki:
+// https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
+//
+// The app doesn't need to listen to any events except `update-downloaded`
+//
+// Uncomment any of the below events to listen for them.  Also,
+// look in the previous section to see them being used.
+//-------------------------------------------------------------------
+// app.on('ready', function()  {
+//   autoUpdater.checkForUpdates();
+// });
+// autoUpdater.on('checking-for-update', () => {
+// })
+// autoUpdater.on('update-available', (info) => {
+// })
+// autoUpdater.on('update-not-available', (info) => {
+// })
+// autoUpdater.on('error', (err) => {
+// })
+// autoUpdater.on('download-progress', (progressObj) => {
+// })
+// autoUpdater.on('update-downloaded', (info) => {
+//   autoUpdater.quitAndInstall();  
+// })
